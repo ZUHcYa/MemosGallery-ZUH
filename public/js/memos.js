19b73be9d25f533a4e5b2f2adfa0cac41e480bd5
ZUHcYa <100796584+ZUHcYa@users.noreply.github.com>
2023-05-28 09:32:03 +0200

    Furhter Clean Up

--- public/js/memos.js
+++ public/js/memos.js
@@ -10,47 +10,17 @@ function photos() {
     .then((data) => {
       let html = "",
         imgs = [];
-      data.data.forEach((item) => {
-        
+      data.data.forEach((item) => {    
         item.resourceList.forEach((resource) => {
           imgs = imgs.concat(domain+"/o/r/"+resource.id+"/"+resource.publicId+"/"+resource.filename);
-
-        });
-        
-        
+        });     
       });
       imgs.forEach((img) => {
-
-
         html += `<div><img src="`+img+`" loading="lazy" alt="..." />`;
         html += `</div>`;
       });
 
       document.querySelector(".grid").innerHTML = html;
-      window.Lately && Lately.init({ target: ".photo-time" });
     })
     .catch();
-
-
 }
-
-$(document).ready(function () {
-  $(".arrow").click(function(){
-    $(".bg").remove();
-    $(".text").remove();
-    $(window).scroll();
-  })
-  $(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 1000) {
-      $("#back-to-top").fadeIn();
-    } else {
-      $("#back-to-top").fadeOut();
-    }
-  });
-
-  $("#back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 800);
-    return false;
-  });
-});
